d682e255b1a248fc5fb7a03379055f2aab8acf25
kosnown <161015191+dandyfn@users.noreply.github.com>
2024-05-21 14:50:29 +0700

    Create KeretaDalamKereta.java

---
+++ KeretaDalamKereta.java
@@ -0,0 +1,94 @@
+package tugasakhirpbo;
+
+import java.util.Random;
+
+public class KeretaDalamKereta extends Deskripsi000 {
+     private String nama = "Kereta Dalam Kereta";
+     private int hentakan;
+     private int suhu;
+     private int terkaman;
+     private int radiasi;
+     private int bising;
+     private int racun;
+    public void aturhentakan(){
+        
+        int batasatas = 20; 
+        int angkaacak = batasatas;
+        this.hentakan=angkaacak;
+      
+    }
+    
+    public void atursuhu(){
+        Random acak = new Random();
+        int batasatas = 10; 
+        int batasbawah = 1; 
+      
+        int angkaacak = acak.nextInt(batasatas-batasbawah);
+        this.suhu=angkaacak;
+    }
+    
+    public void aturterkaman(){
+        Random acak = new Random();
+        int batasatas = 2; 
+        int batasbawah = 1; 
+      
+        int angkaacak = acak.nextInt(batasatas-batasbawah);
+        this.terkaman=angkaacak;
+    }
+    
+    public void aturradiasi(){
+        Random acak = new Random();
+        int batasatas = 20; 
+        int batasbawah = 1; 
+      
+        int angkaacak = acak.nextInt(batasatas-batasbawah);
+        this.radiasi=angkaacak;
+    }
+    
+    public void aturbising(){
+        Random acak = new Random();
+        int batasatas = 20; 
+        int batasbawah = 1; 
+      
+        int angkaacak = acak.nextInt(batasatas-batasbawah);
+        this.bising=angkaacak;
+    }
+    
+    public void aturracun(){
+        Random acak = new Random();
+        int batasatas = 2; 
+        int batasbawah = 1; 
+      
+        int angkaacak = acak.nextInt(batasatas-batasbawah);
+       this.racun=angkaacak;
+    }
+    public int lihathentakan(){
+        return hentakan;
+    }
+    
+    public int lihatsuhu(){
+        return suhu;
+    }
+    
+    public int lihatterkaman(){
+        return terkaman;
+    }
+    
+    public int lihatradiasi(){
+        return radiasi;
+    }
+    
+    public int lihatbising(){
+        return bising;
+    }
+    
+    public int lihatracun(){
+        return racun;
+    }
+    public int kerusakan(){
+        return lihatbising()+lihathentakan()+lihatracun()+lihatradiasi()+lihatsuhu()+lihatterkaman();
+    }
+    public String lihatnama(){
+        return nama;
+    }
+}
