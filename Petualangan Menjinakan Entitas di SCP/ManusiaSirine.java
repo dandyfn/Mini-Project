62347bc7b5e0b07dfa3aa1c88fa1fdb3e04502bf
kosnown <161015191+dandyfn@users.noreply.github.com>
2024-05-21 14:56:38 +0700

    Create ManusiaSirine.java

---
+++ Petualangan Menjinakan Entitas di SCP/ManusiaSirine.java
@@ -0,0 +1,96 @@
+package tugasakhirpbo;
+
+import java.util.Random;
+
+public class ManusiaSirine extends Deskripsi000{
+     private String nama = "Manusia Sirine";
+     private int hentakan;
+     private int suhu;
+     private int terkaman;
+     private int radiasi;
+     private int bising;
+     private int racun;
+    public void aturhentakan(){
+        Random acak = new Random();
+        int batasatas = 20; 
+        int batasbawah = 1; 
+      
+        int angkaacak = acak.nextInt(batasatas-batasbawah);
+        this.hentakan=angkaacak;
+      
+    }
+    
+    public void atursuhu(){
+        Random acak = new Random();
+        int batasatas = 20; 
+        int batasbawah = 1; 
+      
+        int angkaacak = acak.nextInt(batasatas-batasbawah);
+        this.suhu=angkaacak;
+    }
+    
+    public void aturterkaman(){
+        Random acak = new Random();
+        int batasatas = 20; 
+        int batasbawah = 1; 
+      
+        int angkaacak = acak.nextInt(batasatas-batasbawah);
+        this.terkaman=angkaacak;
+    }
+    
+    public void aturradiasi(){
+        Random acak = new Random();
+        int batasatas = 2; 
+        int batasbawah = 1; 
+      
+        int angkaacak = acak.nextInt(batasatas-batasbawah);
+        this.radiasi=angkaacak;
+    }
+    
+    public void aturbising(){
+        Random acak = new Random();
+        int batasatas = 20; 
+        int batasbawah = 1; 
+      
+        int angkaacak = acak.nextInt(batasatas-batasbawah);
+        this.bising=angkaacak;
+    }
+    
+    public void aturracun(){
+        Random acak = new Random();
+        int batasatas = 20; 
+        int batasbawah = 1; 
+      
+        int angkaacak = acak.nextInt(batasatas-batasbawah);
+       this.racun=angkaacak;
+    }
+    public int lihathentakan(){
+        return hentakan;
+    }
+    
+    public int lihatsuhu(){
+        return suhu;
+    }
+    
+    public int lihatterkaman(){
+        return terkaman;
+    }
+    
+    public int lihatradiasi(){
+        return radiasi;
+    }
+    
+    public int lihatbising(){
+        return bising;
+    }
+    
+    public int lihatracun(){
+        return racun;
+    }
+    public int kerusakan(){
+        return lihatbising()+lihathentakan()+lihatracun()+lihatradiasi()+lihatsuhu()+lihatterkaman();
+    }
+    public String lihatnama(){
+        return nama;
+    }
+}
